cfc81e35cedfe055751eb02ab0579513e92647e6
Sandra Irene Bonilla Albistegui <sandra.bonilla@minisistemas.mx>
2024-08-05 13:28:10 -0600

    Se creo una nueva rama

--- EjemploProyecto1/Program.cs
+++ EjemploProyecto1/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hola 050824!");
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ejemplo2");
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ejemplo3");
